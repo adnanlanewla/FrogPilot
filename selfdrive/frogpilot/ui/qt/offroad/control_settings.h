hoomoose/openpilot

Increase max speed faster on short presses
Added toggle to increase the value of the max speed by 5 instead of 1 for Toyota/Lexus and a custom set value for other makes.

selfdrive/frogpilot/ui/qt/offroad/control_settings.h
@@ -39,7 +39,7 @@ private:
   std::set<QString> lateralTuneKeys = {"ForceAutoTune"};
   std::set<QString> longitudinalTuneKeys = {"AccelerationProfile", "AggressiveAcceleration", "DecelerationProfile"};
   std::set<QString> mtscKeys = {};
-  std::set<QString> qolKeys = {"DisableOnroadUploads", "HigherBitrate"};
+  std::set<QString> qolKeys = {"CustomCruise", "DisableOnroadUploads", "HigherBitrate", "ReverseCruise"};
   std::set<QString> speedLimitControllerKeys = {};
   std::set<QString> speedLimitControllerControlsKeys = {};
   std::set<QString> speedLimitControllerQOLKeys = {};

adnanlanewla/FrogPilot

selfdrive/frogpilot/ui/qt/offroad/control_settings.h
@@ -39,7 +39,7 @@ private:
   std::set<QString> lateralTuneKeys = {"ForceAutoTune"};
   std::set<QString> longitudinalTuneKeys = {"AccelerationProfile", "AggressiveAcceleration", "DecelerationProfile"};
   std::set<QString> mtscKeys = {};
-  std::set<QString> qolKeys = {"DisableOnroadUploads"};
+  std::set<QString> qolKeys = {"CustomCruise", "CustomCruiseLong", "DisableOnroadUploads", "ReverseCruise"};
   std::set<QString> speedLimitControllerKeys = {};
   std::set<QString> speedLimitControllerControlsKeys = {};
   std::set<QString> speedLimitControllerQOLKeys = {};